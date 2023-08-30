f1dfae9dbb57bd3a55911b449bc687913f37404f
Ezequiel Sirne <ezequielsirne@gmail.com>
2023-08-30 16:16:14 -0300

    Update params.js

--- src/services/params.js
+++ src/services/params.js
@@ -1,2 +1,2 @@
 // export const urlbase = "https://localhost:7216";
-export const urlbase = "http://sdtsoftware-001-site14.atempurl.com";
+export const urlbase = "http://larosalinaapi.ezequielsirne.ar";
